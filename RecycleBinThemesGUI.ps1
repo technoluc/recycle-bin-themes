14bcbacb0cbe42cd8358536a608326e23cc4e5e3
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2024-01-25 14:56:17 +0100

    Update RecycleBinThemesGUI.ps1
    add event handlers to update img1path with selected_theme preview. also set minimum window size

--- RecycleBinThemesGUI.ps1
+++ RecycleBinThemesGUI.ps1
@@ -1,12 +1,29 @@
-# De URL van het aangepaste icoon
-$iconPath = "https://raw.githubusercontent.com/technoluc/winutil/main/icons/TL.png"
+# Variables
 
-# De URL van de GitHub-repo voor thema's
+# URL of TechnoLuc logo
+$iconPath = "https://github.com/technoluc/recycle-bin-themes/raw/main/assets/TL.png"
+# URL of TechnoLuc's recycle-bin-themes repository
 $repoUrl = "https://api.github.com/repos/technoluc/recycle-bin-themes/contents/themes"
-
-# De URL van img1
+# URL img1
 $img1Path = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/assets/bin.jpg"
 
+
+# Function for error handling
+trap {
+    Write-Host "Er is een fout opgetreden: $_"
+    break
+}
+
+# Function to show feedback
+function ShowMessage($message) {
+    [System.Windows.MessageBox]::Show($message, "Notification", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Information)
+}
+
+
+# Get themes from GitHub
+
+# Importeer de TextInfo-klasse om ToTitleCase te kunnen gebruiken
+Add-Type -AssemblyName System.Globalization
 
 # Haal de lijst met thema's op van de GitHub-repo
 $themesJson = Invoke-RestMethod -Uri $repoUrl
@@ -14,55 +31,50 @@ $themesJson = Invoke-RestMethod -Uri $repoUrl
 # Maak een lege hashtable om de themanamen op te slaan, waarbij de GitHub-namen worden gekoppeld aan de gewenste weergavenamen
 $themeNames = @{}
 
-# Voeg feedback toe aan de gebruiker
-function ShowMessage($message) {
-  [System.Windows.MessageBox]::Show($message, "Notification", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Information)
-}
-
-# Importeer de TextInfo-klasse om ToTitleCase te kunnen gebruiken
-Add-Type -AssemblyName System.Globalization
-
 # Loop door de lijst met thema's en voeg ze toe aan de array
 foreach ($theme in $themesJson) {
-  # Hier krijgen we de naam van het thema uit de "name" eigenschap van het thema-object
-  $themeName = $theme.name
-  # Write-Host "Verwerken van thema: $themeName"
-  if ($theme.type -eq "dir") {
-    # Hier gebruiken we ToTitleCase om de eerste letter van elk woord te kapitaliseren
-    $textInfo = [System.Globalization.CultureInfo]::CurrentCulture.TextInfo
-    $displayName = $textInfo.ToTitleCase($themeName) -replace '-', ' '
-    $themeNames[$themeName] = $displayName
-  }
+    # Hier krijgen we de naam van het thema uit de "name" eigenschap van het thema-object
+    $themeName = $theme.name
+    # Write-Host "Verwerken van thema: $themeName"
+    if ($theme.type -eq "dir") {
+        # Hier gebruiken we ToTitleCase om de eerste letter van elk woord te kapitaliseren
+        $textInfo = [System.Globalization.CultureInfo]::CurrentCulture.TextInfo
+        $displayName = $textInfo.ToTitleCase($themeName) -replace '-', ' '
+        $themeNames[$themeName] = $displayName
+    }
 }
-# Definieer de functie om het standaardpictogram van de prullenbak te wijzigen
+
+# Functions to set the recycle bin icon
 function writeToDefaultIconRegistry {
-  param (
-    $name,
-    $value 
-  )
-  Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "$name" -Value "$value"
+    param (
+        $name,
+        $value 
+    )
+    Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "$name" -Value "$value"
 }
 
 function Set-RecycleBinIcon {
-  param (
-    $emptyIconPath,
-    $fullIconPath
-  )
+    param (
+        $emptyIconPath,
+        $fullIconPath
+    )
   
-  try {
-    # Instellingen voor standaardpictogrammen
-    writeToDefaultIconRegistry "(Default)" "$emptyIconPath,0"
-    writeToDefaultIconRegistry "full" "$fullIconPath,0"
-    writeToDefaultIconRegistry "empty" "$emptyIconPath,0"
+    try {
+        # Instellingen voor standaardpictogrammen
+        writeToDefaultIconRegistry "(Default)" "$emptyIconPath,0"
+        writeToDefaultIconRegistry "full" "$fullIconPath,0"
+        writeToDefaultIconRegistry "empty" "$emptyIconPath,0"
       
-    # Herstart Windows Verkenner om de wijzigingen door te voeren
-    Stop-Process -Name explorer -Force
-    ShowMessage "Recycle Bin Theme set to: $selectedDisplayName."
-  }
-  catch {
-    Write-Host "Fout bij het instellen van het pictogram: $_"
-  }
+        # Herstart Windows Verkenner om de wijzigingen door te voeren
+        Stop-Process -Name explorer -Force
+        ShowMessage "Recycle Bin Theme set to: $selectedDisplayName."
+    }
+    catch {
+        Write-Host "Fout bij het instellen van het pictogram: $_"
+    }
 }
+
+# UI - Window Configuration 
 
 # Maak een WPF-venster voor themaselectie
 Add-Type -AssemblyName PresentationFramework
@@ -73,9 +85,13 @@ $window.Width = 750  # Pas de breedte aan zoals gewenst
 $window.Height = 600  # Pas de hoogte aan zoals gewenst
 $window.WindowStartupLocation = "CenterScreen"  # Centreren op het scherm // Manual / CenterScreen
 # $window.WindowState = [System.Windows.WindowState]::Maximized  # Maximaliseer het venster bij het starten
-
+$window.MinWidth = 500  # Set the minimum width
+$window.MinHeight = 530  # Set the minimum height
 # Stel het venster-icoon in op het aangepaste icoon
 $window.Icon = [System.Windows.Media.Imaging.BitmapFrame]::Create([System.Uri]::new($iconPath))
+
+
+# UI - Grid row and column configuration
 
 # Voeg een Grid-element toe aan het venster om de lay-out te beheren
 $grid = New-Object Windows.Controls.Grid
@@ -100,6 +116,9 @@ $row2 = New-Object Windows.Controls.RowDefinition
 $row2.Height = [Windows.GridLength]::new(1, [Windows.GridUnitType]::Star)
 $grid.RowDefinitions.Add($row2)
 
+
+# UI - Image configuraton
+
 # Voeg een img1 toe aan het venster
 $img1 = New-Object Windows.Controls.Image
 $img1.Source = [System.Windows.Media.Imaging.BitmapImage]::new([System.Uri]"$img1Path")
@@ -107,30 +126,25 @@ $img1.Margin = "10"
 $img1.Width = 400 # Pas de breedte van het img1 hier aan
 $img1.Height = 400 # Pas de hoogte van het img1 hier aan
 
-# Voeg een knop "Standaard" toe aan het venster
-$defaultButton = New-Object Windows.Controls.Button
-$defaultButton.Content = "Default"
-$defaultButton.Margin = "10"
-$defaultButton.Add_Click({
-    try {
-      # Standaardpictogrammen instellen
-      writeToDefaultIconRegistry "empty" "%SystemRoot%\System32\imageres.dll,-55"
-      writeToDefaultIconRegistry "full" "%SystemRoot%\System32\imageres.dll,-54"
-      
-      # Herstart Windows Verkenner om de wijzigingen door te voeren
-      Stop-Process -Name explorer -Force
+# MouseDown event handler for the main window (to reset preview and clear selection)
+$window.Add_MouseDown({
+    # Reset img1 source to the default when any part of the window is clicked
+    $img1.Source = [System.Windows.Media.Imaging.BitmapImage]::new([System.Uri]$img1Path)
+    # Clear selection in the ListBox
+    $listBox.SelectedIndex = -1
 
-      ShowMessage "Recycle Bin restored to default theme."
-    }
-    catch {
-      Write-Host "Fout bij het herstellen van de standaardpictogrammen: $_"
-    }
-  })
+})
+
+
+# UI - TextBlock configuration
 
 # Voeg een tekstblok toe aan het venster
 $textBlock = New-Object Windows.Controls.TextBlock
 $textBlock.Text = 'Select a theme and press "Apply"'
 $textBlock.Margin = "10"
+
+
+# UI- Listbox configuration
 
 # Voeg een lijstvak toe aan het venster
 $listBox = New-Object Windows.Controls.ListBox
@@ -139,43 +153,89 @@ $listBox.Margin = "10"
 # Voeg themanamen toe aan het lijstvak (gebruik de gewenste weergavenaam) en sorteer ze alfabetisch
 $sortedThemeNames = $themeNames.Values | Sort-Object
 foreach ($themeName in $sortedThemeNames) {
-  $listBox.Items.Add($themeName) > $null
+    $listBox.Items.Add($themeName) > $null
 }
 
-# Voeg een knop "Toepassen" toe aan het venster
+# Add a SelectionChanged event handler for the ListBox (to show $selectedThemeName preview)
+$listBox.Add_SelectionChanged({
+    $selectedIndex = $listBox.SelectedIndex
+    if ($selectedIndex -ge 0) {
+        $selectedDisplayName = $listBox.SelectedItem
+        $selectedThemeName = $themeNames.GetEnumerator() | Where-Object { $_.Value -eq $selectedDisplayName } | Select-Object -ExpandProperty Key
+
+        # Get the URL for the preview image
+        $previewImagePath = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/$selectedThemeName/preview.gif"
+
+        # Update the $img1 with the preview image
+        try {
+            $img1.Source = [System.Windows.Media.Imaging.BitmapImage]::new([System.Uri]$previewImagePath)
+        }
+        catch {
+            Write-Host "Error updating preview image: $_"
+        }
+    }
+})
+
+
+# UI - Buttons configuration
+
+# "Apply button configuration"
 $applyButton = New-Object Windows.Controls.Button 
 $applyButton.Content = "Apply"
 $applyButton.Margin = "10"
 $applyButton.Add_Click({
-    $choice = $listBox.SelectedIndex + 1
-    if ($choice -gt 0) {
-      # Haal de geselecteerde thema-naam op basis van de weergavenaam
-      $selectedDisplayName = $listBox.SelectedItem
-      $selectedThemeName = $themeNames.GetEnumerator() | Where-Object { $_.Value -eq $selectedDisplayName } | Select-Object -ExpandProperty Key
+        $choice = $listBox.SelectedIndex + 1
+        if ($choice -gt 0) {
+            # Haal de geselecteerde thema-naam op basis van de weergavenaam
+            $selectedDisplayName = $listBox.SelectedItem
+            $selectedThemeName = $themeNames.GetEnumerator() | Where-Object { $_.Value -eq $selectedDisplayName } | Select-Object -ExpandProperty Key
 
-      # Haal de geselecteerde thema-URL's op
-      $selected_theme = $selectedThemeName
-      $empty_icon_url = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/$selected_theme/$selected_theme-empty.ico"
-      $full_icon_url = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/$selected_theme/$selected_theme-full.ico"
+            # Haal de geselecteerde thema-URL's op
+            $selected_theme = $selectedThemeName
+            $empty_icon_url = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/$selected_theme/$selected_theme-empty.ico"
+            $full_icon_url = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/$selected_theme/$selected_theme-full.ico"
 
-      # Download de iconen naar een tijdelijke map
-      $tempPath = [System.IO.Path]::GetTempPath()
-      $empty_icon_path = Join-Path -Path $tempPath -ChildPath "$selected_theme-empty.ico"
-      $full_icon_path = Join-Path -Path $tempPath -ChildPath "$selected_theme-full.ico"
+            # Download de iconen naar een tijdelijke map
+            $tempPath = [System.IO.Path]::GetTempPath()
+            $empty_icon_path = Join-Path -Path $tempPath -ChildPath "$selected_theme-empty.ico"
+            $full_icon_path = Join-Path -Path $tempPath -ChildPath "$selected_theme-full.ico"
 
-      try {
-        # Download de iconen
-        Invoke-WebRequest -Uri $empty_icon_url -OutFile $empty_icon_path
-        Invoke-WebRequest -Uri $full_icon_url -OutFile $full_icon_path
+            try {
+                # Download de iconen
+                Invoke-WebRequest -Uri $empty_icon_url -OutFile $empty_icon_path
+                Invoke-WebRequest -Uri $full_icon_url -OutFile $full_icon_path
 
-        # Roep de functie aan om pictogrammen in te stellen
-        Set-RecycleBinIcon -emptyIconPath $empty_icon_path -fullIconPath $full_icon_path
-      }
-      catch {
-        Write-Host "Fout bij het downloaden en instellen van het pictogram: $_"
-      }
-    }
-  })
+                # Roep de functie aan om pictogrammen in te stellen
+                Set-RecycleBinIcon -emptyIconPath $empty_icon_path -fullIconPath $full_icon_path
+            }
+            catch {
+                Write-Host "Fout bij het downloaden en instellen van het pictogram: $_"
+            }
+        }
+    })
+
+# "Default button configuration"
+$defaultButton = New-Object Windows.Controls.Button
+$defaultButton.Content = "Default"
+$defaultButton.Margin = "10"
+$defaultButton.Add_Click({
+        try {
+            # Standaardpictogrammen instellen
+            writeToDefaultIconRegistry "empty" "%SystemRoot%\System32\imageres.dll,-55"
+            writeToDefaultIconRegistry "full" "%SystemRoot%\System32\imageres.dll,-54"
+      
+            # Herstart Windows Verkenner om de wijzigingen door te voeren
+            Stop-Process -Name explorer -Force
+
+            ShowMessage "Recycle Bin restored to default theme."
+        }
+        catch {
+            Write-Host "Fout bij het herstellen van de standaardpictogrammen: $_"
+        }
+    })
+
+
+# UI - Grid: add Elements & placement
 
 # Voeg de elementen toe aan het venster
 $grid.Children.Add($img1) > $null
@@ -200,11 +260,7 @@ $grid.Children.Add($applyButton) > $null
 $window.Content = $grid
 
 
-# Voeg error handling toe
-trap {
-  Write-Host "Er is een fout opgetreden: $_"
-  break
-}
+# UI - Window SizeChanged event handler
 
 # Voeg een event handler toe voor het SizeChanged evenement van het venster
 $window.Add_SizeChanged({
@@ -219,7 +275,7 @@ $window.Add_SizeChanged({
 
     # Bereken de nieuwe grootte van de knoppen op basis van de nieuwe hoogte en breedte
     $buttonSizeFactor = [Math]::Min($newWidth / 800, $newHeight / 600)  # Hier kun je 800 en 600 aanpassen aan de oorspronkelijke grootte van het venster
-    $buttonWidth = 100 * $buttonSizeFactor
+    $buttonWidth = 200 * $buttonSizeFactor
     $buttonHeight = 100 * $buttonSizeFactor
 
     # Pas de breedte en hoogte van de knoppen aan
@@ -229,11 +285,13 @@ $window.Add_SizeChanged({
     $applyButton.Height = $buttonHeight
 
     # Voeg hier verdere aanpassingen van de grootte van elementen toe indien nodig
+    # Debug: show size
+    # Write-Host "Window Height: $newHeight"
+    # Write-Host "Window Width: $newWidth"
 
     # Zorg ervoor dat de elementen opnieuw worden weergegeven
     $window.UpdateLayout()
-  })
-
+})
 
 # Toon het venster
 $window.ShowDialog() > $null
